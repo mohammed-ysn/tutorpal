da415489c5a1f55f2485af03dd3bd65f4a42afda
Mohammed Yaseen <mohammedysn15@gmail.com>
2023-05-18 03:50:37 +0100

    Update truncate string function docstring

--- str_utils.py
+++ str_utils.py
@@ -1,10 +1,10 @@
 def truncate_str(string, max_length=16):
     """
-    Truncates a string if it exceeds the maximum length, placing the ellipsis in the middle.
+    Truncates a string to a specified maximum length and adds ellipsis if necessary.
 
     Args:
-        string (str): The string to truncate.
-        max_length (int): The maximum length of the truncated string.
+        string (str): The input string to truncate.
+        max_length (int): The maximum length of the truncated string. Default is 16.
 
     Returns:
         str: The truncated string with ellipsis if necessary.
